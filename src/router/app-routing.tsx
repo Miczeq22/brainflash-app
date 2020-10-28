2776caa03c65855d48632137b8fb168166286f45
Mikołaj Wargowski <mikoajw20@gmail.com>
2020-10-28 21:25:03 +0100

    🌈 Added fix for router

--- src/router/app-routing.tsx
+++ src/router/app-routing.tsx
@@ -1,6 +1,5 @@
 import { DeckCreatorPage } from '@pages/deck-creator/deck-creator.page';
 import { DeckDetailsPage } from '@pages/deck-details/deck-details.page';
-import { HomePage } from '@pages/home/home.page';
 import { LoginPage } from '@pages/login/login.page';
 import { RegisterPage } from '@pages/register/register.page';
 import React from 'react';
@@ -11,22 +10,27 @@ import { DeckCreatorProvider } from '@providers/deck-creator.provider';
 import { YourDecks } from '@pages/your-decks/your-decks.page';
 import { EnrolledDecks } from '@pages/enrolled-decks/enrolled-decks.component';
 import { DeckDetailsProvider } from '@providers/deck-details.provider';
+import { HomePage } from '@pages/home/home.page';
 
 export const AppRoutes = () => (
   <Switch>
-    <GuestRoute path="/login" exact component={LoginPage} />
-    <GuestRoute path="/register" exact component={RegisterPage} />
-    <AuthenticatedRoute path="/" component={HomePage}>
-      <AuthenticatedRoute path="/your-decks" exact component={YourDecks} />
-      <DeckDetailsProvider>
-        <AuthenticatedRoute path="/deck-details/:id" exact component={DeckDetailsPage} />
-      </DeckDetailsProvider>
-      <AuthenticatedRoute path="/enrolled-decks" exact component={EnrolledDecks} />
-      <DeckCreatorProvider>
-        <AuthenticatedRoute path="/deck-creator" exact component={DeckCreatorPage} />
-      </DeckCreatorProvider>
-
-      <AuthenticatedRoute path="*" component={HomePage} />
-    </AuthenticatedRoute>
+    <GuestRoute path="/login" component={LoginPage} />
+    <GuestRoute path="/register" component={RegisterPage} />
+    <AuthenticatedRoute
+      path="/"
+      render={() => (
+        <>
+          <DeckCreatorProvider>
+            <AuthenticatedRoute path="/deck-creator" component={DeckCreatorPage} />
+          </DeckCreatorProvider>
+          <DeckDetailsProvider>
+            <AuthenticatedRoute exact path="/deck-details/:id" component={DeckDetailsPage} />
+          </DeckDetailsProvider>
+          <AuthenticatedRoute path="/enrolled-decks" component={EnrolledDecks} />
+          <AuthenticatedRoute path="/your-decks" component={YourDecks} />
+          <AuthenticatedRoute exact path="/" component={HomePage} />
+        </>
+      )}
+    />
   </Switch>
 );
